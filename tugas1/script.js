2a6f2ab1c832552b95d765465721277b5affc021
puspitasari <puspitasari22232@gmail.com>
2024-04-18 14:28:25 +0700

    kuis2

--- tugas1/script.js
+++ tugas1/script.js
@@ -2,5 +2,14 @@ let nama = "Arif";
 let hobi = ["Baca", "Bermaingame", "Belajar Coding"]
 let listmhs = document.getElementById("listhobi")
 
+document.getElementById("nama").innerHTML = "Arif";
+document.getElementById("hobi").innerHTML = "belajar coding";
+
+data.forEach(showhobi);
+
+function showhobi(velue, index){
+    console.log(velue);
+    listhobi.innerHTML += "<li>" + velue + "</li>";
+}
 
 
---
+++ tugas2/script.js
@@ -0,0 +1,4 @@
+let npm = 2226240001;
+let nama = "Arif";
+let nilai = [80, 70, 90, 85, 60, 88];
+
